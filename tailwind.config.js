6dba132f48e235e052d476620136da6b1b1aa34e
Jahanara Akter <jahanaraaktersuma100@gmail.com>
2024-01-05 10:10:28 +0600

    e-commerce project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      Josefin: ["Josefin Sans"],
+      Lato: ["Lato"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
